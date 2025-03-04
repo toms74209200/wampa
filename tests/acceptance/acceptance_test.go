8a031e1152c53dc2d4ad4dcbf71d2c5a6fb08c8a
toms74209200 <motomotomato@gmail.com>
2025-03-04 16:36:44 +0000

    ✅ Implement acceptance tests for local file monitoring

---
+++ tests/acceptance/acceptance_test.go
@@ -0,0 +1,22 @@
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/cucumber/godog"
+)
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"../../features/local_file_monitoring.feature"},
+			TestingT: t,
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
